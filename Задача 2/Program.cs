503b91e143cb550a5770386ea6d79f058c3978dd
mastiaha <mastiaha@mail.ru>
2021-12-29 18:15:33 +0300

    Добавьте файлы проекта.

---
+++ Задача 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение без знаков препинания");
+            string sl = Console.ReadLine();
+            
+            string[] slova = sl.Split(new[] { " " }, StringSplitOptions.None);
+
+            foreach (string a in slova)
+            {
+                string revers = string.Join("", a.ToUpper().Reverse());
+                if (revers==a)
+                {
+                    Console.WriteLine("Предложение является полиндромом");
+                    break;
+                }
+                else
+                    Console.WriteLine("Предложение не является полиндромом");
+                break;
+            }
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение без знаков препинания");
+            string sl = Console.ReadLine();
+            string max = string.Empty;
+            string[] slova = sl.Split(new[] { " " }, StringSplitOptions.None);
+
+            for (int i = 0; i < slova.Length; i++)
+            {
+                if (slova[i].Length > max.Length)
+                {
+                    max = slova[i];
+                }
+            }
+            Console.WriteLine("Самым длинным словом в предложении будет {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
