3be51924a2cb2f76b7f9951d635c0ca6dc8dc11d
mastiaha <mastiaha@mail.ru>
2021-12-29 18:32:56 +0300

    Решение

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -10,7 +10,25 @@ namespace Задача_2
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите предложение без знаков препинания");
+            string sl = Console.ReadLine();
+
+            string[] slova = sl.Split(new[] { " " }, StringSplitOptions.None);
+
+            foreach (string a in slova)
+            {
+                string revers = string.Join("", a.ToUpper().Reverse());
+                if (revers == a)
+                {
+                    Console.WriteLine("Предложение является полиндромом");
+                    break;
+                }
+                else
+                    Console.WriteLine("Предложение не является полиндромом");
+                break;
+            }
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+            Console.ReadKey();
         }
     }
 }
